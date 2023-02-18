8e018b2d51b605269767ded7cbbf0a0c7ccd8720
LeeSangMun <zbqm1919@naver.com>
2023-02-18 21:30:58 +0900

    Test Commit

---
+++ app/src/main/java/com/example/happypet/FragmentHome.java
@@ -0,0 +1,30 @@
+package com.example.happypet;
+
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.StringTokenizer;
+
+public class FragmentHome extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/happypet/MainActivity.java
+++ app/src/main/java/com/example/happypet/MainActivity.java
@@ -1,16 +1,58 @@
 package com.example.happypet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentHome fragmentHome;
+    FragmentMedical fragmentMedical;
+    FragmentUser fragmentUser;
+    FragmentList fragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Hello", "Happy_Pet");
+
+        fragmentHome = new FragmentHome();
+        fragmentMedical = new FragmentMedical();
+        fragmentList = new FragmentList();
+        fragmentUser = new FragmentUser();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentHome).commit();
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentHome).commit();
+
+                        return true;
+                    case R.id.tab2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentMedical).commit();
+
+                        return true;
+                    case R.id.tab3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentList).commit();
+
+                        return true;
+                    case R.id.tab4:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentUser).commit();
+
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
