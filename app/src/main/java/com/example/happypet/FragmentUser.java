50447bc9a103ee0c2e44d9d44f011c396813e63a
byeongjun98 <degea98@gmail.com>
2023-02-21 23:58:24 +0900

    내 정보 탭 23.02.21 23:56

--- app/src/main/java/com/example/happypet/FragmentUser.java
+++ app/src/main/java/com/example/happypet/FragmentUser.java
@@ -1,9 +1,11 @@
 package com.example.happypet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +16,7 @@ public class FragmentUser extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_user, container, false);
-
+        
         return rootView;
     }
 }
