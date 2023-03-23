fdd3894cf4d1a6ae4d3c4638d0adcfd6477ba711
byeongjun98 <degea98@gmail.com>
2023-03-23 12:03:28 +0900

    23.03.23 내 정보 관리, 주소 관리, 결제수단 관리 버튼에 이미지 추가 +내 정보 관리에 EditText 추가 +주소 관리, 결제수단 관리 +리뷰작성 layout 생성

--- app/src/main/java/com/example/happypet/ReviewActicity.java
+++ app/src/main/java/com/example/happypet/ReviewActicity.java
@@ -1,11 +1,24 @@
 package com.example.happypet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ReviewActicity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        Button revieweditbutton = findViewById(R.id.reviewEdit);
+        revieweditbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplication().getApplicationContext(),write_review_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
