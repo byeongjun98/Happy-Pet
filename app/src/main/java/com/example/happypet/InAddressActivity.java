c1d7996008d2e501df5d2d5c6c3aac9dcbcb769e
byeongjun98 <degea98@gmail.com>
2023-04-13 10:45:37 +0900

    23.04.13 주소관리 액티비티 추가 (웹뷰로 주소검색 api 가져올 예정)

--- app/src/main/java/com/example/happypet/InAddressActivity.java
+++ app/src/main/java/com/example/happypet/InAddressActivity.java
@@ -1,11 +1,25 @@
 package com.example.happypet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class InAddressActivity extends Activity {
+    LinearLayout addAddressButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inmyadress);
+
+        addAddressButton =findViewById(R.id.addAddressButton);
+        addAddressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplication().getApplicationContext(),searchAddressActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
