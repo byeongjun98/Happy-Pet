8818d4a4c3a3daf336ce51cdef4bda76e7f093b5
byeongjun98 <degea98@gmail.com>
2023-05-02 17:46:51 +0900

    23.05.02 결제수단- 카드 추가 액티비티 구성

--- app/src/main/java/com/example/happypet/IncreditActivity.java
+++ app/src/main/java/com/example/happypet/IncreditActivity.java
@@ -1,12 +1,27 @@
 package com.example.happypet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class IncreditActivity extends Activity {
+    LinearLayout addCreditButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.incredit);
+
+        addCreditButton =findViewById(R.id.addCreditButton);
+
+        addCreditButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication().getApplicationContext(),addCreditCard.class);
+                        startActivity(intent);
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/happypet/addCreditCard.java
@@ -0,0 +1,15 @@
+package com.example.happypet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class addCreditCard extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addcreditcard);
+
+    }
+}
