4b9da8ceae14d4d5376a91a3c4cd621136bfdd02
byeongjun98 <degea98@gmail.com>
2023-05-09 13:59:21 +0900

    23.05.09 주소 api 거의 구현됨

--- app/src/main/java/com/example/happypet/searchAddressActivity.java
+++ app/src/main/java/com/example/happypet/searchAddressActivity.java
@@ -2,14 +2,31 @@ package com.example.happypet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.Nullable;
 
 public class searchAddressActivity extends Activity {
+    private WebView webView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchaddress);
 
+        webView = findViewById(R.id.daum_webview);
+
+        // JavaScript 활성화
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
+        // WebViewClient 설정
+        webView.setWebViewClient(new WebViewClient());
+
+        // URL 로드
+        webView.loadUrl("file:///android_asset/postcode.html");
     }
 }
